203512a79eb0ee7e2e100aee785729bf61076a48
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-10-07 14:27:26 +0000

    urrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
 	devtools: { enabled: false },
 	css: ['~/assets/css/index.css'],
 	app: {
-		baseURL: 'https://github.com/ai-shappath/ai-shappath.github.io/',
+		baseURL: '/ai-shappath.github.io/',
 		pageTransition: {
 			name: 'fade',
 			mode: 'out-in',
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 				{
 					rel: 'icon',
 					type: 'image/x-icon',
-					href: 'https://github.com/ai-shappath/ai-shappath.github.io/favicon.ico',
+					href: '/ai-shappath.github.io/favicon.ico',
 				},
 			],
 		},
