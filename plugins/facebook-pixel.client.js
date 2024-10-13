5765295e387d520b765aa63d1c80a84fc3b3148c
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-10-13 11:20:24 +0000

    pixel fix2

--- plugins/facebook-pixel.client.js
+++ plugins/facebook-pixel.client.js
@@ -1,41 +1,48 @@
 /* eslint-disable */
-export default ({ app, store }) => {
-/*
-    ** Only run on client-side and only in production mode
-    */
-// if (process.env.NODE_ENV !== "production") return
-/*
-    ** Initialize Facebook Pixel Script
-    */
-if (process.browser) {
-    !(function(f, b, e, v, n, t, s) {
-    if (f.fbq) return;
-    n = f.fbq = function() {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = !0;
-    n.version = "2.0";
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = !0;
-    t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s);
-    })(
-    window,
-    document,
-    'script',
-    'https://connect.facebook.net/en_US/fbevents.js'
-    );
-    fbq('init', '1623671601696404')
-    app.router.afterEach((to, from) => {
-        /*
-        ** Fire a page view on each route change
-        */
-        console.log("fire pageview", store.state)
-        fbq("track", "PageView")
-    })
+export default defineNuxtPlugin((nuxtApp) => {
+    /*
+     ** Only run on client-side and only in production mode
+     */
+    // if (process.env.NODE_ENV !== "production") return
+  
+    /*
+     ** Initialize Facebook Pixel Script
+     */
+    if (process.client) {
+      !(function(f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function() {
+          n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = "2.0";
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(
+        window,
+        document,
+        'script',
+        'https://connect.facebook.net/en_US/fbevents.js'
+      );
+      fbq('init', '1623671601696404')
+  
+      // Wait for the router to be ready before setting up the afterEach hook
+      nuxtApp.hooks.hook('app:created', () => {
+        nuxtApp.$router.afterEach((to, from) => {
+          /*
+           ** Fire a page view on each route change
+           */
+          console.log("fire pageview")
+          fbq("track", "PageView")
+        })
+      })
     }
-}
+  })
+
+  
