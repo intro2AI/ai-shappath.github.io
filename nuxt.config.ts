8c75a25b0b7c83a4c13db39eea735cdadf2ec2a6
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-10-13 10:57:35 +0000

    facebook pixel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
 	devtools: { enabled: false },
 	css: ['~/assets/css/index.css'],
+	plugins: [
+		{ src: '~/plugins/facebook-pixel.client.js', mode: 'client' }
+	  ],
 	app: {
 		baseURL: '',
 		pageTransition: {
@@ -20,6 +23,29 @@ export default defineNuxtConfig({
 					href: 'favicon.ico',
 				},
 			],
+			script: [
+				{
+				  hid: 'fb-pixel',
+				  innerHTML: `
+					!function(f,b,e,v,n,t,s)
+					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					n.queue=[];t=b.createElement(e);t.async=!0;
+					t.src=v;s=b.getElementsByTagName(e)[0];
+					s.parentNode.insertBefore(t,s)}(window, document,'script',
+					'https://connect.facebook.net/en_US/fbevents.js');
+					fbq('init', '1623671601696404');
+					fbq('track', 'PageView');
+				  `,
+				},
+			  ],
+			noscript: [
+				{
+				innerHTML: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1623671601696404&ev=PageView&noscript=1"/>',
+				body: true
+				}
+			]
 		},
 	},
 	modules: ['@vueuse/nuxt', '@nuxt/image', '@nuxt/content'],
