320dc7ffa20a342312d4cc6116f12728444dad56
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-10-30 12:40:39 +0000

    changes

--- pages/c1/1.vue
+++ pages/c1/1.vue
@@ -18,19 +18,19 @@
 		<main>
 			<div class="content">
 				<p><br />
-					In this section will immerse ourselves into Generative AI Chatbots, also known as Large Language Models(LLMs).
-					We will learn through examples, the kind of tasks an LLM is capable of doing, and how you can fit LLMs into workflows and processes in your domain of interest.
+					In this section we will immerse ourselves into Generative AI Chatbots, also known as Large Language Models(LLMs).
+					We will understand through examples, the kinds of tasks an LLM is capable of doing, and how you can fit LLMs into workflows and processes in your domain of interest.
 					<br /></p>
 				
-				<p>Playground<br/>
-				<a href='https://notebooklm.google/' style='text-decoration: underline;'>NotebookLM</a>: For chatting with books and webpages<br/>
-				<a href='https://claude.ai/new' style='text-decoration: underline;'>Claude</a>: For chatting and "translating" one thing into another<br/>
-				<a href='https://goblin.tools/' style='text-decoration: underline;'>Goblin Tools</a>: A set of very useful tools<br/>
-				<a href='https://build.nvidia.com/nvidia/nemotron-4-mini-hindi-4b-instruct' style='text-decoration: underline;'>Nemotron Hindi</a>: A Language Model taylored for the Hindi Language<br/>
-				<a href='https://huggingface.co/l3cube-pune/marathi-gpt-gemma-7b' style='text-decoration: underline;'>Gemma Marathi</a>: A Language Model taylored for the Marathi Language<br/>
-				<a href='https://aistudio.google.com/prompts/new_chat' style='text-decoration: underline;'>Google AI Studio</a>: for quick fine tuning<br/>
-				<a href='https://gemini.google.com/gems/view' style='text-decoration: underline;'>Google Gemini</a>: Sync accross your Google workspace<br/>
-				<a href='https://www.deeplearning.ai/short-courses/practical-multi-ai-agents-and-advanced-use-cases-with-crewai/' style='text-decoration: underline;'>Multi Agent Systems: Course</a>: Automate Tasks which take in fuzzy inputs<br/>
+				<p>Playground<br/><br/>
+				<a href='https://notebooklm.google/' style='text-decoration: underline;'>NotebookLM</a>: For chatting with books and webpages<br/><br/>
+				<a href='https://claude.ai/new' style='text-decoration: underline;'>Claude</a>: For chatting and "translating" one thing into another<br/><br/>
+				<a href='https://goblin.tools/' style='text-decoration: underline;'>Goblin Tools</a>: A set of very useful tools<br/><br/>
+				<a href='https://build.nvidia.com/nvidia/nemotron-4-mini-hindi-4b-instruct' style='text-decoration: underline;'>Nemotron Hindi</a>: A Language Model taylored for the Hindi Language<br/><br/>
+				<a href='https://huggingface.co/l3cube-pune/marathi-gpt-gemma-7b' style='text-decoration: underline;'>Gemma Marathi</a>: A Language Model taylored for the Marathi Language<br/><br/>
+				<a href='https://aistudio.google.com/prompts/new_chat' style='text-decoration: underline;'>Google AI Studio</a>: for quick fine tuning<br/><br/>
+				<a href='https://gemini.google.com/gems/view' style='text-decoration: underline;'>Google Gemini</a>: Sync accross your Google workspace<br/><br/>
+				<a href='https://www.deeplearning.ai/short-courses/practical-multi-ai-agents-and-advanced-use-cases-with-crewai/' style='text-decoration: underline;'>CrewAI Multi Agent Systems</a>: Automate complex tasks which take in fuzzy inputs<br/><br/>
 				</p>
 			</div>
 		</main>
